03d1d5800a021e89cd72e6dd77b0bd9c8919e708
Charlie025x <charliealonso2001@gmail.com>
2022-08-10 23:11:17 -0500

    finishing stripe payment

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -14,11 +14,42 @@ const PaymentForm = () => {
     if (!stripe || !elements) {
       return;
     }
+
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
+      headers: {
+        "Conent-Type": "application/jason",
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
+
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+
+    console.log(client_secret);
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "charlie test",
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment Successful");
+      }
+    }
   };
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Creadit Card Payment: </h2>
         <CardElement />
         <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay now</Button>
